88824c97dc46f6232432c1d800e1168c72255691
lzyaom <2397925443@qq.com>
2022-12-02 21:15:46 +0800

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,15 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { STLLoader } from 'three/addons/loaders/STLLoader.js';
 const scene = new THREE.Scene();
 
-const windth = window.innerWidth;
+const width = window.innerWidth;
 const height = window.innerHeight;
-const k = windth / height;
+const k = width / height;
 
 const s = 100;
 
-const camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 100000);
+const camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 0.5, 100000);
 
-camera.position.set(100, 200, 100);
+camera.position.set(3500, 2200, 2200);
 
 camera.lookAt(scene.position);
 
@@ -20,11 +20,11 @@ const renderer = new THREE.WebGLRenderer();
 
 const pointLight = new THREE.PointLight(0xffffff);
 
-pointLight.position.set(200, 400, 200);
+pointLight.position.set(3500, 2200, 2200);
 
 scene.add(pointLight);
 
-renderer.setSize(windth, height);
+renderer.setSize(width, height);
 
 renderer.setClearColor(0xb9d3ff, 1);
 
@@ -67,60 +67,94 @@ const models = [
   {
     url: '/public/model/s15/S15 - S15-BASE.STL',
     postion: [0, 0, 1],
+    children: [1],
   },
   {
     url: '/public/model/s15/S15 - S15_MODULE_40.STL',
     postion: [0, 0, 80],
+    children: [2],
   },
   {
     url: '/public/model/s15/S15 - S15_MK2_DOWN.STL',
     postion: [0, 0, 100],
+    children: [3],
   },
   {
     url: '/public/model/s15/S15 - S15_MODULE_20-1.STL',
     postion: [0, 100, 50],
+    children: [4],
   },
   {
     url: '/public/model/s15/S15 - S15_MK2_UP.STL',
     postion: [0, 50, 100],
+    children: [5],
   },
   {
     url: '/public/model/s15/S15 - S15_MODULE_20.STL',
     postion: [0, 0, 150],
+    children: [6],
   },
   {
     url: '/public/model/s15/S15 - S15_IO.STL',
     postion: [0, 0, 200],
+    children: [],
   },
 ];
+const modelMap = new Map();
 
+let loadModelCount = 0;
 models.forEach((model) => {
   loader.load(model.url, function (geometry) {
-    console.log(geometry);
+    loadModelCount++;
     const material = new THREE.MeshPhongMaterial({
-      color: 0xff5533,
+      // color: 0xff5533,
       specular: 0x111111,
       shininess: 200,
     });
     const mesh = new THREE.Mesh(geometry, material);
 
     // mesh.position.set(...model.postion);
-    // mesh.rotation.set(-Math.PI / 2, 0.3, 0);
     mesh.scale.set(0.1, 0.1, 0.1);
 
     mesh.castShadow = true;
     mesh.receiveShadow = true;
 
-    scene.add(mesh);
+    modelMap.set(model, mesh);
+    if (loadModelCount === models.length) {
+      createGroup();
+    }
   });
 });
 
+const createGroup = () => {
+  const group = new THREE.Object3D();
+  for (const [model, mesh] of modelMap) {
+    const children = model.children;
+    if (children.length) {
+      children.forEach((child) => {
+        mesh.children.push(modelMap.get(models[child]));
+      });
+    }
+    group.add(mesh);
+  }
+
+  group.rotateX(Math.PI / 2);
+  // group.rotateZ(Math.PI / 2);
+  scene.add(group);
+};
+const controls = new OrbitControls(camera, renderer.domElement);
+
 function render() {
+  controls.update();
+  renderer.setSize(width, height);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  const { x, y, z } = camera.position;
+  pointLight.position.set(x, y, z);
   renderer.render(scene, camera);
   requestAnimationFrame(render);
 }
 render();
-const controls = new OrbitControls(camera, renderer.domElement);
 </script>
 
 <template>
