deb5be636cfa55364f5dfc6bcd8037d78d76d93e
lzy <2397925443@qq.com>
2022-12-05 23:09:48 +0800

    chore: 修改环境

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  server: {
+    port: 9000,
+    host: '0.0.0.0',
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [vue()],
 })
