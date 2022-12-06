aed03865d650108fbe7b745ac561dc8c004b58e6
lzy <2397925443@qq.com>
2022-12-06 22:43:34 +0800

    refoctor: 重构 3D 模型的展示和基础功能

---
+++ src/components/ModelView/ModelView.ts
@@ -0,0 +1,293 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
+import { ModelType } from './models'
+type ModelOptions = {
+  el: HTMLElement
+  scale: number
+  background: number
+  models: Array<ModelType>
+  fov?: number
+}
+type JointToAngle = {
+  [k: string]: number
+}
+const createGroup = (
+  node: ModelType,
+  models: Array<ModelType>,
+  parentGroup: THREE.Object3D
+) => {
+  const groupObj = new THREE.Object3D()
+  const group = new THREE.Group()
+  groupObj.name = node.name
+  const mesh = modelMap.get(node)!
+  group.add(mesh)
+  groupObj.add(group)
+  if (node.center) {
+    // 设置旋转中心，否则轴的旋转中心在原点
+    const [x, y, z] = node.center
+    groupObj.position.set(x, y, z)
+    group.position.set(-x, -y, -z)
+  }
+  const children = node.children
+  if (children.length) {
+    children.forEach((child: number) => {
+      createGroup(models[child], models, group)
+    })
+  }
+  parentGroup.add(groupObj)
+}
+const modelMap = new Map<any, THREE.Object3D>()
+
+export default class ModelView {
+  private $el: HTMLElement | null = null
+  private width: number = 0
+  private height: number = 0
+  private scene: THREE.Scene | null = null
+  private camera: THREE.PerspectiveCamera | null = null
+  private axes: THREE.AxesHelper | null = null
+  private pointLight: THREE.PointLight | null = null
+  private render: THREE.WebGLRenderer | null = null
+  private control: OrbitControls | null = null
+  private loader: STLLoader | null = null
+  private loadModelCount: number = 0
+  private models: Array<ModelType> = []
+  private rotateAngle: Array<number> = [0, 0, 0, 0, 0, 0]
+  private $options: ModelOptions
+  scale: number = 1
+  axesSize: number = 400
+  background: number
+  fov: number = 0
+
+  constructor(options: ModelOptions) {
+    this.$options = options
+    this.$el = options.el
+    const { clientWidth, clientHeight } = options.el
+    this.width = clientWidth
+    this.height = clientHeight
+    this.models = options.models
+    this.scale = options.scale
+    this.background = options.background
+    this.fov = options.fov || 45
+    this.init()
+  }
+
+  private init(): void {
+    this.initScene()
+    this.initCamera()
+    this.initGrid()
+    this.initAxes()
+    this.initLight()
+    this.initModel()
+    this.initRender()
+    this.initControl()
+  }
+
+  private initScene(): void {
+    this.scene = new THREE.Scene()
+  }
+
+  private initCamera(): void {
+    const camera = new THREE.PerspectiveCamera(
+      this.fov,
+      this.width / this.height,
+      0.5,
+      100000
+    )
+    camera.position.set(350, 220, 400)
+    camera.lookAt(this.scene!.position)
+    this.camera = camera
+  }
+
+  private initLight(): void {
+    // 环境光
+    const ambientLight = new THREE.AmbientLight(0x111111)
+    this.scene!.add(ambientLight)
+    // 点光
+    const pointLight = new THREE.PointLight(0xffffff)
+    pointLight.position.set(3500, 2200, 2200)
+    this.scene!.add(pointLight)
+    this.pointLight = pointLight
+  }
+
+  private initGrid(): void {
+    const divisions = 40
+    const gridHelper = new THREE.GridHelper(this.axesSize, divisions)
+    this.scene!.add(gridHelper)
+  }
+
+  private initAxes(): void {
+    // 辅助三维坐标系
+    const axesHelper = new THREE.AxesHelper(this.axesSize)
+    this.scene!.add(axesHelper)
+    this.axes = axesHelper
+
+    //#region
+    // axesHelper.rotateY((-90 * Math.PI) / 180)
+    // axesHelper.rotateX((-90 * Math.PI) / 180)
+
+    // 添加坐标系方向指示;
+    /* const dir1 = new THREE.Vector3(axesSize, 0, 0)
+    dir1.normalize()
+    const dir2 = new THREE.Vector3(0, axesSize, 0)
+    dir2.normalize()
+    const dir3 = new THREE.Vector3(0, 0, axesSize)
+    dir3.normalize()
+    const origin = new THREE.Vector3(0, 0, 0)
+
+    const arrowHelper1 = new THREE.ArrowHelper(dir3, origin, axesSize, 0xff0000, 8)
+    const arrowHelper2 = new THREE.ArrowHelper(dir1, origin, axesSize, 0x00ff00, 8)
+    const arrowHelper3 = new THREE.ArrowHelper(dir2, origin, axesSize, 0x0000ff, 8)
+    scene.add(arrowHelper1)
+    scene.add(arrowHelper2)
+    scene.add(arrowHelper3) */
+    //#endregion
+  }
+
+  private initRender(): void {
+    const render = new THREE.WebGLRenderer()
+    render.setClearColor(this.background, 1)
+    render.setPixelRatio(window.devicePixelRatio)
+    render.setSize(this.width, this.height)
+    render.render(this.scene!, this.camera!)
+    this.render = render
+    this.$el?.appendChild(render.domElement)
+  }
+
+  private initControl(): void {
+    this.control = new OrbitControls(this.camera!, this.render!.domElement)
+    this.renderHandler()
+  }
+
+  private initModel(): void {
+    const loader = new STLLoader()
+    this.loader = loader
+    this.models.forEach((model) => {
+      if (model.center) {
+        model.center = model.center.map((item) => item * this.scale)
+      }
+      loader.load(model.path, (geometry) => {
+        this.loadModelCount++
+        const material = new THREE.MeshPhongMaterial({
+          // color: 0xff5533,
+          specular: 0x111111,
+          shininess: 200,
+        })
+        const mesh = new THREE.Mesh(geometry, material)
+
+        mesh.scale.set(this.scale, this.scale, this.scale)
+
+        mesh.castShadow = true
+        mesh.receiveShadow = true
+
+        modelMap.set(model, mesh)
+        if (this.loadModelCount === this.models.length) {
+          this.initGroup()
+        }
+      })
+    })
+  }
+
+  private initGroup(): void {
+    createGroup(this.models[0], this.models, this.scene!)
+    // if (props.model === 's15') {
+    //   const jointObj = scene.getObjectByName('base')!
+    //   jointObj.rotateX(Math.PI / 2)
+    // }
+  }
+
+  private renderHandler(): void {
+    this.control!.update()
+    this.camera!.aspect = this.width / this.height
+    this.camera!.updateProjectionMatrix()
+    const { x, y, z } = this.camera!.position
+    this.pointLight!.position.set(x, y, z)
+    this.render!.render(this.scene!, this.camera!)
+    requestAnimationFrame(this.renderHandler.bind(this))
+  }
+
+  /**
+   * 更新机械臂各个轴的角度
+   * @param angles 每个轴的角度
+   */
+  update(angles: Array<number>): void {
+    const jointToAngleMap: JointToAngle = angles.reduce(
+      (prev, item, i: number) => {
+        const j = i + 1
+        if (item !== this.rotateAngle[i] && !prev[j]) {
+          prev[j] = item
+        }
+        return prev
+      },
+      {} as JointToAngle
+    )
+    const keys = Object.keys(jointToAngleMap)
+
+    if (keys.length === 0) {
+      // 没有改变关节角度
+      return
+    }
+    for (let i = 0; i < keys.length; i++) {
+      const joint = Number(keys[i])
+      const jointObj = this.scene!.getObjectByName('J' + joint)!
+      const { x, y, z } = this.models[joint].rotate!
+      const axis = new THREE.Vector3(x, y, z)
+      // jointObj?.rotateOnAxis(
+      //   axis.normalize(),
+      //   (jointToAngleMap[joint] * Math.PI) / 180
+      // )
+      jointObj?.setRotationFromAxisAngle(
+        axis.normalize(),
+        (jointToAngleMap[joint] * Math.PI) / 180
+      )
+    }
+    this.rotateAngle = angles
+    this.renderHandler()
+  }
+
+  /**
+   * 放大视图
+   */
+  zoomIn(): void {
+    this.fov += 5
+    this.camera!.fov = this.fov
+    this.renderHandler()
+  }
+
+  /**
+   * 缩小视图
+   */
+  zoomOut(): void {
+    this.fov -= 5
+    this.camera!.fov = this.fov
+    this.renderHandler()
+  }
+  /**
+   * 重置视图
+   */
+  zoomReset(): void {
+    this.camera!.fov = this.$options.fov!
+    this.renderHandler()
+  }
+
+  /**
+   *
+   */
+  destory(): void {}
+
+  /**
+   * 重置模型
+   * @param models 新模型
+   */
+  reset(models: ModelType[]): void {
+    this.loadModelCount = 0
+    this.rotateAngle = [0, 0, 0, 0, 0, 0]
+    modelMap.clear()
+    this.$el!.removeChild(this.render!.domElement)
+    this.render!.clearStencil()
+    this.render!.dispose()
+    this.scene!.remove()
+    this.models = models
+    this.init()
+  }
+}
--- src/components/ModelView/models.ts
+++ src/components/ModelView/models.ts
@@ -1,124 +1,109 @@
 type AxisType = 'x' | 'y' | 'z'
 
 export type ModelType = {
-  url: string
+  path: string
   name: string
-  postion: Array<number>
   children: Array<number>
-  center?: Record<AxisType, number>
+  center?: Array<number>
   rotate?: Record<AxisType, number>
 }
 
 export const models: Record<string, Array<ModelType>> = {
   s15: [
     {
-      url: '/public/model/s15/S15 - S15-BASE.STL',
+      path: '/public/model/s15/S15 - S15-BASE.STL',
       name: 'base',
-      postion: [0, 0, 1],
       children: [1],
     },
     {
-      url: '/public/model/s15/S15 - S15_MODULE_40.STL',
+      path: '/public/model/s15/S15 - S15_MODULE_40.STL',
       name: 'J1',
-      postion: [0, 0, 80],
       children: [2],
-      center: { x: 0, y: 54, z: 0 },
+      center: [0, 54, 0],
       rotate: { x: 0, y: 1, z: 0 },
     },
     {
-      url: '/public/model/s15/S15 - S15_MK2_DOWN.STL',
+      path: '/public/model/s15/S15 - S15_MK2_DOWN.STL',
       name: 'J2',
-      postion: [0, 0, 100],
       children: [3],
-      center: { x: 0, y: 30, z: 0 },
+      center: [0, 30, 0],
       rotate: { x: 0, y: 0, z: -1 },
     },
     {
-      url: '/public/model/s15/S15 - S15_MODULE_20-1.STL',
+      path: '/public/model/s15/S15 - S15_MODULE_20-1.STL',
       name: 'J3',
-      postion: [0, 100, 50],
       children: [4],
-      center: { x: 0, y: 900, z: 0 },
+      center: [0, 900, 0],
       rotate: { x: 0, y: 0, z: 1 },
     },
     {
-      url: '/public/model/s15/S15 - S15_MK2_UP.STL',
+      path: '/public/model/s15/S15 - S15_MK2_UP.STL',
       name: 'J4',
-      postion: [0, 50, 100],
       children: [5],
-      center: { x: 0, y: 74, z: 0 },
+      center: [0, 74, 0],
       rotate: { x: 0, y: 1, z: 0 },
     },
     {
-      url: '/public/model/s15/S15 - S15_MODULE_20.STL',
+      path: '/public/model/s15/S15 - S15_MODULE_20.STL',
       name: 'J5',
-      postion: [0, 0, 150],
       children: [6],
-      center: { x: 0, y: 1174, z: 0 },
+      center: [0, 1174, 0],
       rotate: { x: 1, y: 0, z: 0 },
     },
     {
-      url: '/public/model/s15/S15 - S15_IO.STL',
+      path: '/public/model/s15/S15 - S15_IO.STL',
       name: 'J6',
-      postion: [0, 0, 200],
       children: [],
-      center: { x: 0, y: 1194, z: 0 },
+      center: [0, 1194, 0],
       rotate: { x: 1, y: 0, z: 0 },
     },
   ],
   common: [
     {
-      url: '/public/model/common/Elfin_BASE_n.STL',
+      path: '/public/model/common/Elfin_BASE_n.STL',
       name: 'base',
-      postion: [0, 0, 1],
       children: [1],
     },
     {
-      url: '/public/model/common/Elfin_MODULE_25_n.STL',
+      path: '/public/model/common/Elfin_MODULE_25_n.STL',
       name: 'J1',
-      postion: [0, 0, 80],
       children: [2],
-      center: { x: 0, y: 74, z: 0 },
+      center: [0, 74, 0],
       rotate: { x: 0, y: 1, z: 0 },
     },
     {
-      url: '/public/model/common/Elfin_MK2_DOWN_n.STL',
+      path: '/public/model/common/Elfin_MK2_DOWN_n.STL',
       name: 'J2',
-      postion: [0, 0, 100],
       children: [3],
-      center: { x: 0, y: 220, z: 0 },
+      center: [0, 220, 0],
       rotate: { x: 0, y: 0, z: -1 },
     },
     {
-      url: '/public/model/common/Elfin_MODULE_20_n.STL',
+      path: '/public/model/common/Elfin_MODULE_20_n.STL',
       name: 'J3',
-      postion: [0, 100, 50],
       children: [4],
-      center: { x: 0, y: 600, z: 0 },
+      center: [0, 600, 0],
       rotate: { x: 0, y: 0, z: 1 },
     },
     {
-      url: '/public/model/common/Elfin_UP_n.STL',
+      path: '/public/model/common/Elfin_UP_n.STL',
       name: 'J4',
-      postion: [0, 50, 100],
       children: [5],
-      center: { x: 0, y: 724, z: 0 },
+      center: [0, 724, 0],
       rotate: { x: 0, y: 1, z: 0 },
     },
     {
-      url: '/public/model/common/Elfin_MODULE_14_n.STL',
+      path: '/public/model/common/Elfin_MODULE_14_n.STL',
       name: 'J5',
-      postion: [0, 0, 150],
       children: [6],
-      center: { x: 0, y: 1020, z: 0 },
+      center: [0, 1020, 0],
       rotate: { x: 0, y: 0, z: 1 },
     },
     {
-      url: '/public/model/common/Elfin_IO_n.STL',
+      path: '/public/model/common/Elfin_IO_n.STL',
       name: 'J6',
-      postion: [0, 0, 200],
       children: [],
-      center: { x: 0, y: 1126, z: 0 },
+      center: [0, 1126, 0],
       rotate: { x: 0, y: 1, z: 0 },
     },
   ],
